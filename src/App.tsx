7b0b5f53aba5db6a73649f40db8fafa42e222701
jpollar4 <jpollar4@gmail.com>
2022-12-06 10:22:50 -0500

    test push

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App = ({ signOut }: { signOut?: () => void }) => {
 
 	return (
 		<View className="App">
-			<Heading level={1}>My Notes App</Heading>
+			<Heading level={1}>My Notes App is neat</Heading>
 			<View as="form" margin="3rem 0" onSubmit={createNote}>
 				<Flex direction="row" justifyContent="center">
 					<TextField
